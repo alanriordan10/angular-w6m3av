22e9411f6711bfcf339de454e134c04ed7f82d68
alanriordan10 <alanriordan10@gmail.com>
2020-02-10 14:38:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-w6m3av
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-w6m3av)
---
+++ src/app/listeners/listeners.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listeners',
+  templateUrl: './listeners.component.html',
+  styleUrls: ['./listeners.component.css']
+})
+export class ListenersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
